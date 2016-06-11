80fb4d4278464825b34ca77404630135ad741a2a
Bob <bob.baxley@gmail.com>
2016-06-11 17:33:07 -0400

    using pandas to munge historical price data

--- ystockquote.py
+++ ystockquote.py
@@ -15,6 +15,8 @@
 
 __version__ = '0.2.5dev'
 
+import pandas as pd
+
 try:
     # py3
     from urllib.request import Request, urlopen
@@ -466,8 +468,7 @@ def get_historical_prices(symbol, start_date, end_date):
     Get historical prices for the given ticker symbol.
     Date format is 'YYYY-MM-DD'
 
-    Returns a nested dictionary (dict of dicts).
-    outer dict keys are dates ('YYYY-MM-DD')
+    Returns a pandas dataframe
     """
     params = urlencode({
         's': symbol,
@@ -481,20 +482,5 @@ def get_historical_prices(symbol, start_date, end_date):
         'ignore': '.csv',
     })
     url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % params
-    req = Request(url)
-    resp = urlopen(req)
-    content = str(resp.read().decode('utf-8').strip())
-    daily_data = content.splitlines()
-    hist_dict = dict()
-    keys = daily_data[0].split(',')
-    for day in daily_data[1:]:
-        day_data = day.split(',')
-        date = day_data[0]
-        hist_dict[date] = \
-            {keys[1]: day_data[1],
-             keys[2]: day_data[2],
-             keys[3]: day_data[3],
-             keys[4]: day_data[4],
-             keys[5]: day_data[5],
-             keys[6]: day_data[6]}
-    return hist_dict
+
+    return pd.read_csv(url)
